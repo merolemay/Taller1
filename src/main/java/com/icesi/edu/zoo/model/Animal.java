ed8e591ea9d1204dda8ba915bd27457aa35ac46b
Diego H <d13g01hl@gmail.com>
2022-09-19 13:20:46 -0500

    Little changes

--- src/main/java/com/icesi/edu/zoo/model/Animal.java
+++ src/main/java/com/icesi/edu/zoo/model/Animal.java
@@ -1,18 +1,35 @@
 package com.icesi.edu.zoo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 import java.util.UUID;
 
+@Data
+@Table(name = "`animal`")
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Animal {
 
     @Id
+    @Type(type="org.hibernate.type.UUIDCharType")
     private UUID id;
 
     private String name;
 
+    @Type(type="org.hibernate.type.UUIDCharType")
     private UUID maleParent;
 
+    @Type(type="org.hibernate.type.UUIDCharType")
     private UUID femaleParent;
 
     private boolean sex;
