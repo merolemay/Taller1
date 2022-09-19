42b41b0ff62ee16cb707dfc615d2befdfae92a72
Diego H <d13g01hl@gmail.com>
2022-09-19 13:10:50 -0500

    Added API

---
+++ src/main/java/com/icesi/edu/zoo/api/AnimalAPI.java
@@ -0,0 +1,21 @@
+package com.icesi.edu.zoo.api;
+
+import com.icesi.edu.zoo.dto.AnimalDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("/animal")
+public interface AnimalAPI {
+
+    @GetMapping("/{animalId}")
+    AnimalDTO getAnimal(@PathVariable UUID animalId);
+
+    @PostMapping()
+    AnimalDTO createAnimal(@RequestBody AnimalDTO animalDTO);
+
+    @GetMapping()
+    List<AnimalDTO> getAnimals();
+
+}
