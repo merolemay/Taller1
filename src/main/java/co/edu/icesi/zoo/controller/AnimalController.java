a0062d5621935b4f0e0b45abc7863dc10e8bf3e7
nicolaspenagos <nicolas.penagosm98@gmail.com>
2022-09-24 11:58:59 -0500

    Done

--- src/main/java/co/edu/icesi/zoo/controller/AnimalController.java
+++ src/main/java/co/edu/icesi/zoo/controller/AnimalController.java
@@ -87,15 +87,7 @@ public class AnimalController implements AnimalZooAPI {
 
         if(parentId!=null&&!parentId.matches(UtilConstant.UUID_REGEX))
             AnimalExceptionUtils.throwAnimalException(HttpStatus.BAD_REQUEST, AnimalErrorCode.CODE_06, AnimalErrorMsgs.INVALID_ID);
-
-       /*
-        try{
-
-            if(parentId!=null) UUID.fromString(parentId);
-
-        }catch (Exception e){
-
-        }*/
+        
     }
 
 }
